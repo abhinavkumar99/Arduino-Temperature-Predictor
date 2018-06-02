96412bfbd9be8967c6287dd8bca9a6bafb718258
abhinavkumar99 <abhinavk99@gmail.com>
2018-06-02 16:46:32 -0400

    Add files via upload

---
+++ dht-test.ino
@@ -0,0 +1,25 @@
+#include <DHT.h>
+
+#define DHTPIN 2
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
+int chk;
+float hum;
+float temp;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  hum = dht.readHumidity();
+  temp = dht.readTemperature();
+
+  Serial.print(hum);
+  Serial.print(" ");
+  Serial.println(temp);
+}
